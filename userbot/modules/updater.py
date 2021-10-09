fc6c0860f2752787faa0b7eb810725c4ac0778ee
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-10-09 19:14:37 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -100,7 +100,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if BOTLOG:
             await event.client.send_message(
                 BOTLOG_CHATID, "#NOTE \n"
-                "𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓🔥 Berhasil Di Update✓")
+                "Cangcut-Userbot🔥 Berhasil Di Update✓")
 
     else:
         await event.edit('`[HEROKU]:'
@@ -117,9 +117,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓** Berhasil Di Update✓')
+    await event.edit('**Cangcut-Userbot** Berhasil Di Update✓')
     await asyncio.sleep(1)
-    await event.edit('`𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓 Di Restart....`')
+    await event.edit('`Cangcut-Userbot Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Silahkan Tunggu Beberapa Detik..`')
     await asyncio.sleep(10)
@@ -190,13 +190,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n`⚡𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓⚡\n` sudah **versi terbaru**\n`BRANCH:`**{UPSTREAM_REPO_BRANCH}**\n')
+            f'\n`🚀Cangcut-Userbot🚀\n` sudah **versi terbaru**\n`BRANCH:`**{UPSTREAM_REPO_BRANCH}**\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**UPDATE Terbaru Untuk ⚡𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓⚡ [{ac_br}]:\n\nPERUBAHAN:**\n`{changelog}`'
+        changelog_str = f'**UPDATE Terbaru Untuk 🚀Cangcut-Userbot🚀 [{ac_br}]:\n\nPERUBAHAN:**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog is too big, view the file to see it.`")
             file = open("output.txt", "w+")
@@ -216,10 +216,10 @@ async def upstream(event):
         await event.edit(
             '`Force-Syncing to latest stable userbot code, please wait...`')
     else:
-        await event.edit('`Proses Update 𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓, ....🛠️`')
-        await event.edit('`Proses Update 𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓, loading....🛠️`')
-        await event.edit('`Proses Update 𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓, updating....🛠️`')
-        await event.edit('`Proses Update 𝐏𝐔𝐒𝐒𝐘-𝐔𝐒𝐄𝐑𝐁𝐎𝐓\nSilahkan tunggu....🛠️`')
+        await event.edit('`Proses Update Cangcut-Userbot, ....🛠️`')
+        await event.edit('`Proses Update Cangcut-Userbot, loading....🛠️`')
+        await event.edit('`Proses Update Cangcut-Userbot, updating....🛠️`')
+        await event.edit('`Proses Update Cangcut-Userbot\nSilahkan tunggu....🛠️`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
